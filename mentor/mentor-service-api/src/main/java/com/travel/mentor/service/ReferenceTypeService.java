db40e12ecf632acbdff0c521179f166231f7d137
Geoff Powdrell <geoffpowdrell@gmail.com>
2011-06-04 17:03:44 +1000

    updates

--- mentor/mentor-service-api/src/main/java/com/travel/mentor/service/ReferenceTypeService.java
+++ mentor/mentor-service-api/src/main/java/com/travel/mentor/service/ReferenceTypeService.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public interface ReferenceTypeService {
 
-    List<ReferenceTypeDTO> findAllReferenceTypes(String findAllNamedQuery);
+    List<ReferenceTypeDTO> findAllAccommodationSiteTypes();
+    List<ReferenceTypeDTO> findAllActivitySiteTypes();
+    List<ReferenceTypeDTO> findAllRoomConfigurationTypes();
+    List<ReferenceTypeDTO> findAllRoomTypes();
 
 }
