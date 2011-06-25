53173d5645aee6e51889a05cd571b372b94de108
Geoff Powdrell <geoffpowdrell@gmail.com>
2011-06-25 23:54:02 +1000

    updates

--- mentor/dao/src/main/java/com/travel/mentor/dao/dto/base/ReferenceTypeDTO.java
+++ mentor/dao/src/main/java/com/travel/mentor/dao/dto/base/ReferenceTypeDTO.java
@@ -1,21 +1,8 @@
 package com.travel.mentor.dao.dto.base;
 
-import com.travel.mentor.dao.dto.impl.UserDTO;
-
-import java.io.Serializable;
-import java.sql.Timestamp;
-import java.util.Date;
-
 public class ReferenceTypeDTO extends AbstractAuditedNameDescDTO {
 
-//    protected Long id;
-//    protected String name;
-//    protected String description;
     protected Class entityClass;
-//    protected UserDTO createUserDTO=new UserDTO();
-//    protected Timestamp createDate=new Timestamp(new Date().getTime());
-//    protected UserDTO updateUserDTO=new UserDTO();
-//    protected Timestamp updateDate=new Timestamp(new Date().getTime());
 
     public ReferenceTypeDTO() {}
 
