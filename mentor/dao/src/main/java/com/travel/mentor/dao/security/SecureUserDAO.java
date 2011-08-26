07eb233b205f969e04ffcef3f366feb2635d39bb
Geoff Powdrell <geoffpowdrell@gmail.com>
2011-08-26 21:03:59 +1000

    updates

--- mentor/dao/src/main/java/com/travel/mentor/dao/security/SecureUserDAO.java
+++ mentor/dao/src/main/java/com/travel/mentor/dao/security/SecureUserDAO.java
@@ -2,10 +2,16 @@ package com.travel.mentor.dao.security;
 
 import com.travel.mentor.dao.dto.security.SecureUserDTO;
 
+import java.util.List;
+
 public interface SecureUserDAO {
+
+    List<SecureUserDTO> findAllSecureUsers();
 
     SecureUserDTO findByUsername(String username);
 
     SecureUserDTO saveOrUpdate(SecureUserDTO secureUserDTO, SecureUserDTO loggedOnUser);
+
+    void delete(SecureUserDTO secureUserDTO);
 
 }
--- mentor/dao/src/main/java/com/travel/mentor/dao/security/impl/SecureUserDAOImpl.java
+++ mentor/dao/src/main/java/com/travel/mentor/dao/security/impl/SecureUserDAOImpl.java
@@ -5,13 +5,15 @@ import com.travel.mentor.dao.base.AbstractMentorDAO;
 import com.travel.mentor.dao.dto.security.SecureUserDTO;
 import com.travel.mentor.dao.security.SecureUserDAO;
 import com.travel.mentor.domain.security.SecureUser;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Repository("secureUserDAO")
 @Transactional
@@ -20,34 +22,44 @@ public class SecureUserDAOImpl extends AbstractMentorDAO implements SecureUserDA
     @Resource
     private SecureUserAssembler secureUserAssembler;
 
+    @Override
+    public List<SecureUserDTO> findAllSecureUsers() {
+        List<SecureUser> secureUserList = em.createNamedQuery(SecureUser.FIND_ALL_SECURE_USERS).getResultList();
+        return secureUserAssembler.assembleToDTOList(secureUserList);
+    }
+
+    @Override
+    public void delete(SecureUserDTO secureUserDTO) {
+        SecureUser secureUser = em.find(SecureUser.class, secureUserDTO.getUsername());
+        em.getEntityManagerFactory().getCache().evict(secureUser.getClass(), secureUser.getUsername());
+    }
+
+    @PostConstruct
     protected void cacheDomainObjects() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        List<String> namedQueries = new ArrayList<String>();
+        namedQueries.add(SecureUser.FIND_ALL_SECURE_USERS);
+        super.cacheDomainObjects(namedQueries);
     }
 
     @Override
     public SecureUserDTO findByUsername(String username) {
-        SecureUser secureUser = em.find(SecureUser.class, username);
-        return secureUserAssembler.assembleToDTOInstance(secureUser);
+        return secureUserAssembler.assembleToDTOInstance(em.find(SecureUser.class, username));
     }
 
     @Override
     public SecureUserDTO saveOrUpdate(SecureUserDTO secureUserDTO, SecureUserDTO loggedOnUser) {
-        SecureUser secureUser = secureUserAssembler.assembleToEntityInstance(secureUserDTO);
-
+        SecureUser secureUser;
         if (secureUserDTO.getUsername() == null || em.find(SecureUser.class, secureUserDTO.getUsername()) == null) {
-            secureUser.setCreateUser(loggedOnUser.getUsername());
+            secureUser = secureUserAssembler.assembleToEntityInstance(secureUserDTO);
             secureUser.setCreateDate(new Timestamp(new Date().getTime()));
-            secureUser.setUpdateUser(loggedOnUser.getUsername());
-            secureUser.setUpdateDate(new Timestamp(new Date().getTime()));
-            em.persist(secureUser);
         }
         else {
-            SecureUser existingSecureUser = em.find(SecureUser.class, secureUserDTO.getUsername());
-            BeanUtils.copyProperties(secureUser, existingSecureUser);
-            secureUser.setUpdateUser(loggedOnUser.getUsername());
-            secureUser.setUpdateDate(new Timestamp(new Date().getTime()));
-            em.merge(secureUser);
+            secureUser = em.find(SecureUser.class, secureUserDTO.getUsername());
+            secureUserAssembler.deepCopy(secureUserDTO, secureUser);
         }
+
+        secureUser.setUpdateDate(new Timestamp(new Date().getTime()));
+        em.merge(secureUser);
 
         return secureUserAssembler.assembleToDTOInstance(secureUser);
     }
